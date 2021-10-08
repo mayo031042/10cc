eb1b066f75980d3924f369ef57a9a4b59fb4d9ca
mayo031042 <kurumelon1219@gmail.com>
2021-10-08 21:32:40 +0900

    整数から始まる空白のない加減算

--- 10cc.c
+++ 10cc.c
@@ -15,7 +15,30 @@ int main(int argc, char **argv)
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+
+    // 今回は　正しい入力の場合空白文字がないものとして考え　以下コメントアウトは正しい入力を前提としている
+    // strtolについて　第一引数のポインタのしめすものについて第三引数を基数としたものに変換する　
+    // この場合数値変換できなかった最初のポインタを第二引数に渡す
+    printf("    mov rax, %d\n", strtol(p,&p,10));
+
+    while(*p){
+        if(*p=='+'){
+            // 数値と想定されるものを指すポインタに進む
+            p++;
+            printf("    add rax, %d\n", strtol(p,&p,10));
+            continue;
+        }
+        else if(*p=='-'){
+            p++;
+            printf("    sub rax, %d\n", strtol(p,&p,10));
+            continue;
+        }
+        else {    
+            fprintf(stderr, "入力が間違っています\n");
+            return 1;
+        }
+    }
+
     printf("    ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -18,5 +18,8 @@ assert() {
 
 assert 0 0
 assert 42 42
+assert 10 "4+6"
+assert 2 "14-6-10+4"
+assert 9 "1+2-3+4+5"
 
 echo OK
