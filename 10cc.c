4679c99448c01c646dd8627969c62e7e9cd274ba
mayo031042 <kurumelon1219@gmail.com>
2021-10-08 20:17:01 +0900

    整数一つを受け取れるコンパイラ

--- 10cc.c
+++ 10cc.c
@@ -1,3 +1,18 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        fprintf(stderr, "引数の数が間違っています\n");
+        return 0;
+    }
+
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
+    printf("    mov rax, %d\n", atoi(argv[1]));
+    printf("    ret\n");
+    return 0;
 }
