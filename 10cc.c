5b0648bf0e153be33104635c5337d087596143a7
mayo031042 <kurumelon1219@gmail.com>
2021-10-09 11:48:01 +0900

    tokenize main以外はOK

--- 10cc.c
+++ 10cc.c
@@ -8,7 +8,7 @@
 // tokenの種類を分ける
 typedef enum
 {
-    TK_reserved,
+    TK_RESERVED,
     TK_NUM,
     TK_EOF,
 } TokenKind;
@@ -36,19 +36,20 @@ void error(char *fmt, ...)
     exit(1);
 }
 
-// consume, expect, expect_numberはtokenの解析に用いられる　
+// consume, expect, expect_numberは出来上がったtokenの利用にmain内で用いられる　
 // 期待されるtokenの種類を決め打つことでerrorの判定が楽である
 // 期待どおりでなかった場合の処理は　別の判定の余地の有無で異なる　間違った入力であると
 // 判断できるタイミングでのみerrorを吐きたいので　expect関数は判定群の最後にだけ用いるのが良さそう
 
 // tokenの種類に対応したアセンブリ出力はmain内で行うので　tokenizeの段階では
-// 切り分けからの　種類決め打ちとそれに伴った適切なデータの抽出である
+// 切り分けからの　種類決め打ちとそれに伴った適切な抽出データのnew_tokenへの受け渡しである
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて真を返す。
 // それ以外の場合には偽を返す。
+// expectと合わせて+か-のどちらであるかを判定する
 bool consume(char op)
 {
-    if (token->kind != TK_reserved)
+    if (token->kind != TK_RESERVED || token->str[0] != op)
         return 0;
     token = token->next;
     return 1;
@@ -58,7 +59,7 @@ bool consume(char op)
 // それ以外の場合にはエラーを報告する。
 void expect(char op)
 {
-    if (token->kind != TK_reserved)
+    if (token->kind != TK_RESERVED || token->str[0] != op)
         error("%cではありません\n", op);
     token = token->next;
 }
@@ -80,30 +81,57 @@ bool at_eof()
 }
 
 // 新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, int val, char *str, Token *cur)
+Token *new_token(TokenKind kind, Token *cur, char *str)
 {
     // ここでは雛形と連結リストさえ作れれば良い？
     // 解釈と整理はtokenizeの方で行うのでそのtokenのオブジェクトを 情報を受け取って作れば良さそう
     // Token*型の新しい変数tokを用意し　curの次として設定し tokを新しいオブジェクトとして返す
-    Token *tok;
+    Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
-    tok->val = val;
     tok->str = str;
     cur->next = tok;
     return tok;
 }
 
 // 入力文字列pをトークナイズしてそれを返す
+// 結果的にtoken列はheadとEOFに囲まれることになり　mainでのreturnはheadの次（実質的に先頭）になる
 Token *tokenize(char *p)
 {
     // 今見ているpを適切な範囲で切り取って　tokenの情報を整理する
     // その次に切り分けられるtokenを再帰的にもとめつつ　nextはその返り値が利用できそう
 
-    // token = new_token(token);
+    Token head;
+    head.next = NULL;
+    // tokenize に便利なためcurというTokenを指すポインタを用意する
+    Token *cur = &head;
 
+    // 切り分け　spaceは無視　記号が連続で続いても切り分けのみする
     while (*p)
     {
+        if (isspace(*p))
+        {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-')
+        {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p))
+        {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("tokenizeできません\n");
     }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
 }
 
 int main(int argc, char **argv)
@@ -111,7 +139,7 @@ int main(int argc, char **argv)
     if (argc != 2)
     {
         fprintf(stderr, "引数の数が間違っています\n");
-        return 0;
+        return 1;
     }
 
     char *p = argv[1];
