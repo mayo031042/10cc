d21121d730a941e64771e49ef9360cb196fa6a7f
mayo031042 <kurumelon1219@gmail.com>
2021-10-16 12:56:08 +0900

    parseまでOK

--- 10cc.h
+++ 10cc.h
@@ -31,3 +31,26 @@ void expect(char *op);
 int expect_number();
 Token *tokenize(char *p);
 
+typedef enum
+{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+    ND_EQ,
+    ND_NE,
+    ND_LT,
+    ND_LE,
+} NodeKind;
+
+typedef struct Node Node;
+struct Node
+{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *expr();
--- parse.c
+++ parse.c
@@ -7,3 +7,149 @@
 
 #include "10cc.h"
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *primary();
+Node *unary();
+Node *mul();
+Node *add();
+Node *relational();
+Node *equality();
+// exprのみmain内から用いるため　Hに移動している
+// Node *expr();
+
+Node *primary()
+{
+    if (consume("("))
+    {
+        Node *node = expr();
+        expect(")");
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
+Node *unary()
+{
+    if (consume("+"))
+    {
+        return primary();
+    }
+    else if (consume("-"))
+    {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    else
+        return primary();
+}
+
+Node *mul()
+{
+    Node *node = unary();
+
+    for (;;)
+    {
+        if (consume("*"))
+        {
+            node = new_node(ND_MUL, node, unary());
+        }
+        else if (consume("/"))
+        {
+            node = new_node(ND_DIV, node, unary());
+        }
+        else
+            return node;
+    }
+}
+
+Node *add()
+{
+    Node *node = mul();
+
+    for (;;)
+    {
+        if (consume("+"))
+        {
+            node = new_node(ND_ADD, node, mul());
+        }
+        else if (consume("-"))
+        {
+            node = new_node(ND_SUB, node, mul());
+        }
+        else
+            return node;
+    }
+}
+
+Node *relational()
+{
+    Node *node = add();
+
+    for (;;)
+    {
+        if (consume("<="))
+        {
+            node = new_node(ND_LE, node, add());
+            continue;
+        }
+        else if (consume(">="))
+        {
+            node = new_node(ND_LE, add(), node);
+            continue;
+        }
+        else if (consume("<"))
+        {
+            node = new_node(ND_LT, node, add());
+            continue;
+        }
+        else if (consume(">"))
+        {
+            node = new_node(ND_LT, add(), node);
+            continue;
+        }
+        else
+            return node;
+    }
+}
+
+Node *equality()
+{
+    Node *node = relational();
+
+    for (;;)
+    {
+        if (consume("=="))
+        {
+            node = new_node(ND_EQ, node, relational());
+            continue;
+        }
+        else if (consume("!="))
+        {
+            node = new_node(ND_NE, node, relational());
+            continue;
+        }
+        else
+            return node;
+    }
+}
+
+Node *expr()
+{
+    Node *node = equality();
+}
+
