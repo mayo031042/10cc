23b2cc2be88a6d97f53f4ef81df36ad973f41021
mayo031042 <kurumelon1219@gmail.com>
2021-10-29 13:25:37 +0900

    func_others.c からutils.cに名前変更

---
+++ utils.c
@@ -0,0 +1,67 @@
+#include "10cc.h"
+
+int Queue[Queue_size];
+int Queue_front = 0;
+
+void error(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+void err(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+}
+
+int count()
+{
+    static int cnt = 2;
+    return cnt++;
+}
+
+void clear_semicolon()
+{
+    while (consume(TK_RESERVED, ";"))
+        ;
+}
+
+int qfront()
+{
+    if (!Queue_front)
+        error("ループ構文内ではありません");
+    return Queue[Queue_front];
+}
+
+void qpush(int val)
+{
+    Queue_front++;
+    if (Queue_size <= Queue_front)
+        error("Queue size overflowed.");
+    Queue[Queue_front] = val;
+}
+void qpop()
+{
+    if (!Queue_front)
+        error("Queue size underflowed.");
+    Queue_front--;
+}
