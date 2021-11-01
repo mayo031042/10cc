f015ebf34c45b23c9f63e2f34b76b823f1db6861
mayo031042 <kurumelon1219@gmail.com>
2021-11-01 21:38:32 +0900

    tokens[] を見てnode 作成を補助する関数を　parse.hへ移動

--- 10cc.h
+++ 10cc.h
@@ -48,7 +48,6 @@ typedef enum
     ND_LT,        // <
     ND_LE,        // <=
     ND_ASSIGN,    // =
-    // ND_FUNC_DEF,  // 関数定義
     ND_FUNC_CALL, // 関数呼び出し
     ND_LVAR,      // 変数
     ND_NUM,       // 数値
