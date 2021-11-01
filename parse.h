d2cb28517f505456e4313e62ff9458f7f5fdcec7
mayo031042 <kurumelon1219@gmail.com>
2021-11-01 22:17:10 +0900

    function() をvoid に変更

--- parse.h
+++ parse.h
@@ -28,10 +28,11 @@ Node *new_node_num(int val);
 Node *new_node_ident(LVar *lvar);
 Node *new_node_if();
 Node *new_node_else();
+
 Node *new_node_for();
 Node *new_node_while();
 Node *new_node_do();
-Node *new_node_block();
+
 Node *build_block();
 
 LVar *new_lvar(int max_offset);
