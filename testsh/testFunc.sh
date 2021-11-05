835b39409f23a9968af0f92129bb142258cf7e21
mayo031042 <kurumelon1219@gmail.com>
2021-11-05 21:18:33 +0900

    testsh 分離

---
+++ testsh/testFunc.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# 関数対応開始
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./bin/10cc "$input" > tmp.s
+    gcc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "   $input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assemble(){
+    expected="$1"
+    input="$2"
+
+    ./bin/10cc "$input" > tmp.s
+    echo "compiled"
+
+    # ./optimize
+    exit 0
+}
+
+echo "TEST func"
+assert 42 "
+foo()
+{
+    return 42;
+}
+main()
+{
+    x=foo();
+    return x;
+}"
+
+assert 5 "
+foo11()
+{
+    return 3;
+}
+main()
+{
+    x=1+foo11()+3;
+    return x-2;
+}"
+
+assert 11 "
+plus()
+{
+    return 3+4;
+}
+minus()
+{
+    return 5-2;
+}
+main()
+{
+    x=plus()-minus();
+    return x+plus();
+}"
+
+assert 8 "
+fuga();
+hoge()
+{
+    return fuga()+3;
+}
+fuga()
+{
+    return 5;
+}
+main()
+{
+    return hoge();
+}"
+
+# ネスト位置によって参照すべき変数が違う -> 宣言である　という判断がつく必要がある　int 導入までスルー
+# assert 2 "
+# main()
+# {
+#     ret=0;
+#     x=1;
+#     {
+#         x=2;
+#         {
+#             x=3;    
+#         }
+#         ret=x;
+#     }
+#     return ret;
+# }"
+
+echo -e "\n         You are a god-dammit genius !!\n"
---
+++ testsh/testType.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+# 関数対応開始
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./bin/10cc "$input" > tmp.s
+    gcc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "   $input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assemble(){
+    expected="$1"
+    input="$2"
+
+    ./bin/10cc "$input" > tmp.s
+    echo "compiled"
+
+    # ./optimize
+    exit 0
+}
+
+echo "TEST func"
+
+assert 1 " int main(){ return 1;}"
+assert 1 " int main(){ int x; return 1;}"
+assert 1 " int main(){ int x; x=1; return x;}"
+
+assert 42 "
+int foo()
+{
+    return 42;
+}
+int main()
+{
+    int x;
+    x=foo();
+    return x;
+}"
+
+assert 5 "
+int foo11()
+{
+    return 3;
+}
+int main()
+{
+    int x;
+    x=1+foo11()+3;
+    return x-2;
+}"
+
+assert 11 "
+int plus()
+{
+    return 3+4;
+}
+int minus()
+{
+    return 5-2;
+}
+int main()
+{
+    int x;
+    x=plus()-minus();
+    return x+plus();
+}"
+
+assert 8 "
+int fuga();
+int hoge()
+{
+    return fuga()+3;
+}
+int fuga()
+{
+    return 5;
+}
+int main()
+{
+    return hoge();
+}"
+
+# ネスト位置によって参照すべき変数が違う -> スコープ
+assert 2 "
+int main()
+{
+    int ret; ret=0;
+    int x; x=1;
+    {
+        {
+            int x; x=2;
+            {
+                int x; x=3;    
+            }
+            ret=x;
+        }
+    }
+    return ret;
+}"
+
+assert 1 "
+int main()
+{
+    int i;
+    for(i=0;i<1000;i+=1)
+    {
+        int x;
+    }
+    return 1;
+}
+"
+echo "OK"
+
+assert 1 "
+int foo(int x)
+{
+    return x;
+}
+int main()
+{
+    return foo(1);
+}
+"
+
+assert 6 "
+int plus2(int x)
+{
+    return x+2;
+}
+int main()
+{
+    int x;
+    x=plus2(4);
+    return x;
+}"
+
+assert 34 "
+int fib(int x,int y)
+{
+    if(30 < x+y)return x+y;
+    return fib(y,x+y);
+}
+int main()
+{
+    return fib(1,1);
+}"
+
+assert 5 "
+int foo(int x);
+int main()
+{
+    return foo(2)+3;
+}
+int foo(int x)
+{
+    return x;
+}"
+
+assert 19 "
+int hoge(int x);
+int fuga(int x)
+{
+    if(10<x)return x;
+    return hoge(x+5);
+}
+int hoge(int x)
+{
+    return fuga(x)+1;
+}
+int main()
+{
+    return hoge(0);
+}
+"
+
+echo -e "\n         You are a god-dammit genius !!\n"
