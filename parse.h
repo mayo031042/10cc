935d920d80f478d469374412f85f957987929397
mayo031042 <kurumelon1219@gmail.com>
2021-11-10 16:57:01 +0900

    リファクタリング

--- parse.h
+++ parse.h
@@ -1,15 +1,20 @@
 #include "10cc.h"
 
 // tokens[] を見てnode 作成を補助する関数
+
+// token の判別だけを行いtoken_pos を進めない関数 -> current...
 bool current_token_is(TokenKind kind, char *op);
+bool current_token_is_type();
+
+// token の判別を行いtoken_pos を進める関数 -> consume...
 bool consume(TokenKind kind, char *op);
 bool consume_keyword(TokenKind kind);
 bool consume_ident();
-bool current_token_is_type();
 
+// token の断定を行いtoken_pos を進める関数 -> expect...
 bool expect(TokenKind kind, char *op);
-TypeKind expect_vartype();
 bool expect_ident();
+TypeKind expect_vartype();
 int expect_number();
 
 bool at_eof();
@@ -41,13 +46,17 @@ Node *new_node_do();
 
 Node *build_block();
 
+// 変数を解釈
 LVar *new_lvar();
 LVar *find_lvar_within_block(int depth);
 LVar *find_lvar();
 Node *declare_lvar();
-void declare_arg(); // 関数宣言、定義の際に 引数表をセットする
-Node *build_arg();  // 関数呼び出しのときに　引数をexpr() 解釈して登録先のレジスタを記憶する
-void consume_arg();
 
+// 引数を解釈
+void declare_arg(); // 関数の定義時 引数表を登録する
+void consume_arg(); // 関数の宣言時 引数表を読み飛ばす
+Node *build_arg();  // 関数呼び出しのときに　引数をexpr() 解釈して登録先のレジスタを割り当てる
+
+// 関数を解釈
 int find_func();
 Func *new_func(Token *tok);
