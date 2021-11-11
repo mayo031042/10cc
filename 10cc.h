15a4307bb9410f0217c00915ca4225090e3788ad
mayo031042 <kurumelon1219@gmail.com>
2021-11-11 17:46:04 +0900

    演算子token を切り離してtkopeを作った　代入演算子の名前もへんこう

--- 10cc.h
+++ 10cc.h
@@ -8,8 +8,8 @@
 // token　
 typedef enum
 {
-    TK_OPERATOR,        // 演算子
-    TK_RESERVED,        // +-*/, ==, !=, <, <=
+    TK_OPERATOR,        // 演算子,* ,&
+    TK_RESERVED,        // ==, !=, <, <= ,;()
     TK_ASSIGN_OPERATOR, // 代入演算子 +=,,,
     TK_IDENT,           // 変数
     TK_NUM,             // 数値
