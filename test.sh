d96f2de1aa2674d36872050547fc88d64b77989c
mayo031042 <kurumelon1219@gmail.com>
2021-11-16 19:09:16 +0900

    gcc でも実行して検算

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # test/ に含まれるtest file を実行する　返り値が０であることを確認して終了する
 
+dir_name="test/"
+flag=0
+
 assert()
 {
-    ./bin/10cc "$1"
+    file_name="$1"
+
+    ./bin/10cc "$file_name"
     gcc -o tmp/tmp tmp/tmp.s
     ./tmp/tmp
 
@@ -12,16 +17,31 @@ assert()
     if [ "$actual" = 0 ]; then
         echo " OK"
     else
-        echo "$input => $expected expected, but got $actual"
-        exit 1
+        echo -e "\n  10cc returned $actual...\n"
+        flag=1
+    fi
+
+    gcc -o a "$file_name" 
+    ./a
+    
+    actual="$?"
+
+    if [ "$actual" -ne 0 ]; then
+        echo -e "  $file_name returned $actual...,\n  something is wrong in test case\n"
+        flag=1
     fi
 }
 
-dir_name="test/"
 
 for file_name in `\find $dir_name -maxdepth 1 -type f`; 
     do
         assert $file_name
     done
 
-echo -e "\n         You are a god-dammit genius !!\n"
+if [ $flag = 0 ]; then
+    echo -e "\n         You are a god-dammit genius !!\n"
+else 
+    echo -e "\nsomething was wrong..."
+fi
+
+rm -f a
